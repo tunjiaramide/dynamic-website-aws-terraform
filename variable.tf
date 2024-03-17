986905a0fe398613bee5bf6897cbd92044b71dbf
tunjiaramide <adetunjiaramide@gmail.com>
2024-03-17 06:07:13 +0100

    created vpc file

---
+++ variable.tf
@@ -0,0 +1,43 @@
+#vpc variables
+variable "vpc_cidr" {
+  default     = "10.0.0.0/16"
+  description = "VPC cidr block"
+  type        = string
+}
+
+
+variable "public_subnet_az1_cidr" {
+  default     = "10.0.0.0/24"
+  description = "Public Subnet AZ1 cidr block"
+  type        = string
+}
+
+variable "public_subnet_az2_cidr" {
+  default     = "10.0.1.0/24"
+  description = "Public Subnet AZ2 cidr block"
+  type        = string
+}
+
+variable "private_app_subnet_az1_cidr" {
+  default     = "10.0.2.0/24"
+  description = "Private App Subnet AZ1 cidr block"
+  type        = string
+}
+
+variable "private_app_subnet_az2_cidr" {
+  default     = "10.0.3.0/24"
+  description = "Private App Subnet AZ2 cidr block"
+  type        = string
+}
+
+variable "private_data_subnet_az1_cidr" {
+  default     = "10.0.4.0/24"
+  description = "Private Data Subnet AZ1 cidr block"
+  type        = string
+}
+
+variable "private_data_subnet_az2_cidr" {
+  default     = "10.0.5.0/24"
+  description = "Private Data Subnet AZ2 cidr block"
+  type        = string
+}
